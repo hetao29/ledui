e53d35a7f3279cbc081ae7bf6705ca55a93f1bb0
hetao@hetao.name <hetao@hetao.name@78d2197d-5f19-2318-7aec-da9c5f2d2fbe>
2012-08-08 13:54:31 +0000

    注册，js校验

--- trunk/web/app/user/main.page.php
+++ trunk/web/app/user/main.page.php
@@ -8,8 +8,12 @@ class user_main extends STpl{
 	function pageEntry($inPath){
 		echo $this->render("user/sign.html");
 	}
-	///user.main.login
-	function pageSign($inPath){
+	///user.main.register
+	function pageRegister($inPath){
+		if(!empty($_POST)){
+
+		}else{
+		}
 		echo $this->render("user/sign.html");
 	}
 	///user.main.logout
