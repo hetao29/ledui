0becb03ce22ada01e4f17c4a724de9562e4451ec
hetao@hetao.name <hetao@hetao.name@78d2197d-5f19-2318-7aec-da9c5f2d2fbe>
2012-11-06 03:30:30 +0000

    增加app不会重复运行的问题

--- crontab/main.php
+++ crontab/main.php
@@ -10,36 +10,35 @@ for(;;){
 	$config=SConfig::parse("crontab.conf");
 	foreach($config as $conf){
 		if(!empty($conf->app) || !empty($conf->interval)){
-			if(	
-				//没有开始，或者已经超时
-				(	empty($info[$conf->app]['starttime']) || 
-					$info[$conf->app]['starttime']+$conf->interval<=time()
-				)&&(
-				//没有开始，并且已经结束
-					empty($info[$conf->app]['status']) ||
-					$info[$conf->app]['status']=="end"
-				)
-			){
+			if(	//没有开始，或者已经超时
+				empty($info[$conf->app]['starttime']) || 
+				$info[$conf->app]['starttime']+$conf->interval<=time()
+			  ){
 				$info[$conf->app]['starttime']=time();
-				$info[$conf->app]['status']="start";
-				$pid = pcntl_fork();
+				if(!empty($info[$conf->app]['pid']) && file_exists("/tmp/".$info[$conf->app]['pid'])){
+					//上次运行的进程还没有结束
+					continue;
+				}
+				$ret= pcntl_fork();
 				$command = $conf->app;
 				if(!empty($conf->params)){
 					$params = $conf->params;
 				}else{
 					$params = new stdclass;
 				};
-				if ($pid == -1) {//error
-					continue;
-				} else if ($pid) {//parent
-					continue;
+				if ($ret== -1) {//error
+				} else if ($ret) {//parent
+					$info[$conf->app]['pid']=$ret;
 				} else {//child
+					$cpid = posix_getpid();
+					file_put_contents("/tmp/$cpid","$cpid");
 					SLog::write("Exec $command");
 					include($command);
-					$info[$conf->app]['status']=="end"; //?这里的变量不知道会不会有问题,有可能子进程不会更新父进程里的数据
+					unlink("/tmp/$cpid");
 					exit;
 				}
 			}
 		}
+		pcntl_wait($c_status,WNOHANG);
 	}
 }
